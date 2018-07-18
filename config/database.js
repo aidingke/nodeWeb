00035e6c4faca41604d18848685e0833f3b5f9e8
冯建东 <fengjiandong@mhace.com>
2018-07-18 17:58:26 +0800

    111

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV=="production"){
 	module.exports = 
 	{
-		mongoURL:"mongodb://mister:1234@ds139715.mlab.com:39715/node-app-prod"
+		// mongoURL:"mongodb://mister:1234@ds139715.mlab.com:39715/node-app-prod"
 	}
 }else{
 	module.exports = {mongoURL:"mongodb://localhost/mydbs"}
